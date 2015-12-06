ccdc427c75857ea3d169c515bec858fb8d0017c5
Shinryu <Shinryu>
2015-12-06 17:54:36 +0800

    With testing

--- client.cpp
+++ client.cpp
@@ -5,8 +5,17 @@ int main()
 {
     sf::TcpSocket socket;
     sf::Socket::Status status = socket.connect("172.20.10.3", 53000);
-    if (status != sf::Socket::Done)
-    {
+    if (status != sf::Socket::Done){
         std::cout<< "AHA NOT CONNECTED!" <<std::endl;
+    }
+    else{ //should be in a while loop too to continuously get messages
+        std::cout << "Connected to server" <<std::endl;
+        sf::Packet packet2;
+        if(socket.receive(packet2) == sf::Socket::Done){
+            std::string as;
+            packet2 >> as;
+            std::cout << as << std::endl;
+        }
+        system("PAUSE");
     }
 }
