35e8557ee447b49ff6328198bbebb783353cd3fb
erocrizs <aemielvin.loremia@gmail.com>
2015-12-07 14:25:48 +0800

    Add 2D vector class

---
+++ G-Shift/Vec2.cpp
@@ -0,0 +1,64 @@
+#include "Vec2.h"
+#include <cmath>
+
+Vec2::Vec2(float a, float b)
+{
+    x = a;
+    y = b;
+}
+
+float Vec2::getX()
+{
+    return x;
+}
+
+float Vec2::getY()
+{
+    return y;
+}
+
+Vec2 Vec2::operator+(Vec2& that)
+{
+    Vec2 ret(x+that.getX(), y+that.getY());
+    return ret;
+}
+
+Vec2 Vec2::operator-(Vec2& that)
+{
+    Vec2 ret(x-that.getX(), y-that.getY());
+    return ret;
+}
+
+Vec2 Vec2::operator*(float i)
+{
+    Vec2 ret(x*i, y*i);
+    return ret;
+}
+
+float Vec2::operator*(Vec2& that)
+{
+    return (x*that.getX()) + (y*that.getY());
+}
+
+float Vec2::operator%(Vec2& that)
+{
+    return (x*that.getY())-(y*that.getX());
+}
+
+float Vec2::magnitude()
+{
+    return std::sqrt((x*x)+(y*y));
+}
+
+Vec2 Vec2::normalize()
+{
+    float mag = this->magnitude();
+    Vec2 ret(x/mag, y/mag);
+    return ret;
+}
+
+Vec2 Vec2::perp()
+{
+    Vec2 ret(-y, x);
+    return ret;
+}
---
+++ G-Shift/Vec2.h
@@ -0,0 +1,24 @@
+#ifndef VEC2_H_INCLUDED
+#define VEC2_H_INCLUDED
+
+class Vec2
+{
+private:
+    float x;
+    float y;
+public:
+    float getX();
+    float getY();
+
+    Vec2(float, float);
+    Vec2 operator+ (Vec2&);
+    Vec2 operator- (Vec2&);
+    Vec2 operator* (float);
+    float operator* (Vec2&);
+    float operator% (Vec2&);
+    float magnitude();
+    Vec2 normalize();
+    Vec2 perp();
+};
+
+#endif // VEC2_H_INCLUDED
